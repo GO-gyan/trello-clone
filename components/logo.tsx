9e9ae708a4c509e23a9bb946400995a2872e5021
Gobinda Thakur <gobinda.thakur@outlook.com>
2023-12-03 11:25:19 +0530

    added navbar and footer

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,13 +4,13 @@ import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 
 const headingFont = localFont({
-    src: "../../public/fonts/font.woff2",
+    src: "../public/fonts/font.woff2",
 });
 function Logo() {
     return (
         <Link href="/">
             <div className="hover:opacity-75 transition items-centergap-x-2 hidden md:flex">
-                <Image src={"/logo.svg"} alt="logo" width={100} height={100} />
+                <Image src={"/logo.svg"} alt="logo" width={30} height={30} />
                 <p className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}>Taskify</p>
             </div>
         </Link>
