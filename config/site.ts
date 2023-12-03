5517a2143ae7e91a034ff62a1b14f9c4c617683d
Gobinda Thakur <gobinda.thakur@outlook.com>
2023-12-03 12:06:15 +0530

    added login and sign up and the middleware

---
+++ config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+    name: "Taskify",
+    description: "Task management for teams",
+    url: "https://taskify.dev",
+    ogImage: "https://taskify.dev/og.png",
+    links: {
+        twitter: "https://twitter.com/withastro",
+        github: "https://github.com/withastro/astro",
+    },
+};
